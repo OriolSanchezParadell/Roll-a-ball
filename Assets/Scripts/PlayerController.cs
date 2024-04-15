ddcd7739eb6993acc27ce73378e63ab9978ad5bb
Your Name <you@example.com>
2024-04-15 18:36:16 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Include the namespace required to use Unity UI and Input System
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Create public variables for player speed, and for the Text UI game objects
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject WinAudio;
+
+    private float movementX;
+    private float movementY;
+
+    private Rigidbody rb;
+    private int count;
+    private AudioSource clin;
+
+    // At the start of the game..
+    void Start()
+    {
+        // Assign the Rigidbody component to our private rb variable
+        rb = GetComponent<Rigidbody>();
+
+        // Set the count to zero 
+        count = 0;
+
+        SetCountText();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
+        WinAudio.SetActive(false);
+
+
+        clin = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        // Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            // Add one to the score variable 'count'
+            count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
+
+            clin.Play();
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+        movementY = v.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 13)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+            WinAudio.SetActive(true);
+        }
+    }
+}
